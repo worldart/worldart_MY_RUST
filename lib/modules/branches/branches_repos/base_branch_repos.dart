28a3feb179c8d4faaa553d155912bdf154f2d2f4
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-17 09:54:21 +0000

    Delete lib directory

--- lib/modules/branches/branches_repos/base_branch_repos.dart
+++
@@ -1,6 +0,0 @@
-
-import '../../../models/branch_model.dart';
-
-abstract class BaseBranchRepository {
-  Stream<List<Branch>> getAllBranches();
-}
--- lib/widgets/messenger.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-ScaffoldFeatureController<SnackBar, SnackBarClosedReason> Message(
-    BuildContext context, String message) {
-  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-    duration: const Duration(seconds: 2),
-  ));
-}
